f3b24ae1bcd7c94f63a0e5a03a51575e77916356
Alwafor <alwafor1@gmail.com>
2022-07-01 10:00:09 +0300

    Setup NextJS app, add tailwindcss

---
+++ client-next/pages/index.tsx
@@ -0,0 +1,11 @@
+import type {NextPage} from 'next'
+
+const Home: NextPage = () => {
+    return (
+        <div className={'bg-yellow-500'}>
+            Hello there
+        </div>
+    )
+}
+
+export default Home
