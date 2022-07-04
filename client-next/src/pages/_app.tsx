4ee0d2bfd648d988998a3792471eef756dbb2c00
Alwafor <alwafor1@gmail.com>
2022-07-04 08:47:52 +0300

    Ship styles to the next app version, add sass package

--- client-next/src/pages/_app.tsx
+++ client-next/src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import "../styles/global.css";
 import type { AppProps } from "next/app";
+import "@/assets/styles/styles.scss"
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
