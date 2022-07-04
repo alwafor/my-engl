1417840e4939cb7297c040a29f0a5eac12cbfdf1
Alwafor <alwafor1@gmail.com>
2022-07-04 09:22:23 +0300

    Remove unnecessary semicolon

--- client-next/src/pages/_app.tsx
+++ client-next/src/pages/_app.tsx
@@ -6,6 +6,6 @@ import Navbar from '@/components/layout/Navbar'
 export default function MyApp({ Component, pageProps }: AppProps) {
   return <>
     <Navbar/>
-    <Component {...pageProps} />;
+    <Component {...pageProps} />
   </>
 }
