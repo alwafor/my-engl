b1c13386ad40a4f854264eb6a038c9ea1d81e2ef
Alwafor <alwafor1@gmail.com>
2022-07-09 20:11:01 +0300

    Add count query param to getAllWords endpoint

--- client-next/src/pages/api/word/getAll.ts
+++ client-next/src/pages/api/word/getAll.ts
@@ -1,11 +1,18 @@
 import {NextApiRequest, NextApiResponse} from 'next'
 import {prismaClient} from '../../../../prisma/prismaInstance'
 
+/**
+ * Get words from the whole database, dictionaries are not considered
+ */
 export default async function getAllWords(req: NextApiRequest, res: NextApiResponse) {
-  const allWords = await prismaClient.word.findMany({
+  const count = Number.parseInt(String(req.query.count)) || undefined
+
+  const words = await prismaClient.word.findMany({
     include: {
       translations: true
-    }
+    },
+    take: count
   })
-  return res.status(200).json(allWords)
+
+  return res.status(200).json(words)
 }
