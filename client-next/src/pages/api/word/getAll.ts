325349bb9881aa5721f42b67b720f3ebd957432b
Alwafor <alwafor1@gmail.com>
2022-07-09 20:11:39 +0300

    Rename 'count' to 'take'

--- client-next/src/pages/api/word/getAll.ts
+++ client-next/src/pages/api/word/getAll.ts
@@ -5,13 +5,13 @@ import {prismaClient} from '../../../../prisma/prismaInstance'
  * Get words from the whole database, dictionaries are not considered
  */
 export default async function getAllWords(req: NextApiRequest, res: NextApiResponse) {
-  const count = Number.parseInt(String(req.query.count)) || undefined
+  const take = Number.parseInt(String(req.query.take)) || undefined
 
   const words = await prismaClient.word.findMany({
     include: {
       translations: true
     },
-    take: count
+    take: take
   })
 
   return res.status(200).json(words)
