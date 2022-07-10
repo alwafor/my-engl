c08a04d4871ce95c8a7b4f6c22727d559e001336
Buor <buor0@mail.ru>
2022-07-10 15:36:18 +0300

    Add zod validation to 'addMany' words endpoint

--- client-next/src/pages/api/word/addMany.ts
+++ client-next/src/pages/api/word/addMany.ts
@@ -1,6 +1,20 @@
 import {NextApiRequest, NextApiResponse} from 'next'
+import {z} from 'zod'
 
 export default async function addManyWords(req: NextApiRequest, res: NextApiResponse) {
-  // todo implement createManyWords
-  res.status(400).json({error: 'not implemented yet!'})
+  const words = req.body
+
+  const validationSchema = z.object({
+      word: z.string(),
+      translationsString: z.string()
+    }
+  ).array()
+
+  const validationResult = validationSchema.safeParse(words)
+
+  if(!validationResult.success) {
+    res.status(400).json('Error! Wrong data! Message: ' + validationResult.error)
+  } else {
+    res.status(200).json(validationResult.data)
+  }
 }
