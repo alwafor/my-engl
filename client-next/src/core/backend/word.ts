2e51abf34bc1afa2137563780755186690d997d6
Alwafor <alwafor1@gmail.com>
2022-07-10 11:03:20 +0300

    Move word 'add' logic to separate file to reuse in other route

---
+++ client-next/src/core/backend/word.ts
@@ -0,0 +1,43 @@
+import {prismaClient} from '../../../prisma/prismaInstance'
+import {Prisma} from '@prisma/client'
+
+export const addWord = async (word: string, translationsString: string) => {
+
+  word = word.toLowerCase()
+
+  const translations = translationsString
+    .split(',')
+    .map(tr => tr.trim().toLowerCase())
+
+  // check if this word already exists
+  const wordSearch = await prismaClient.word.findFirst({where: {word}})
+  if(wordSearch) return {isError: true, errorMessage: `Word '${word}' is already exist!`}
+
+  // create translations arrays
+  let translationsIdsToConnectTo: {id: number}[] = []
+  let translationsToCreate: Prisma.TranslationCreateInput[] = []
+
+  // check if similar translations exist
+  for(let translation of translations) {
+    const translationDb = await prismaClient.translation.findFirst({where: {translation}})
+    if(translationDb) {
+      translationsIdsToConnectTo.push({id: translationDb.id})
+    }
+    else {
+      translationsToCreate.push({translation})
+    }
+  }
+
+  // create a new word
+  const wordDb = await prismaClient.word.create({
+    data: {
+      word: word.toLowerCase(),
+      translations: {
+        create: translationsToCreate,
+        connect: translationsIdsToConnectTo
+      }
+    }
+  })
+
+  return {isError: false, data: wordDb}
+}
--- client-next/src/pages/api/word/addOne.ts
+++ client-next/src/pages/api/word/addOne.ts
@@ -1,6 +1,5 @@
 import {NextApiRequest, NextApiResponse} from 'next'
-import {prismaClient} from '../../../../prisma/prismaInstance'
-import {Prisma} from '@prisma/client'
+import {addWord} from '@/core/backend/word'
 
 export default async function addOneWord(req: NextApiRequest, res: NextApiResponse) {
   let {translationsString, word}: { translationsString?: string, word?: string } = req.body
@@ -13,42 +12,7 @@ export default async function addOneWord(req: NextApiRequest, res: NextApiRespon
     return res.status(400).json({error: 'No translations or word in req.body!'})
   }
 
-  word = word.toLowerCase()
-
-  const translations = translationsString
-    .split(',')
-    .map(tr => tr.trim().toLowerCase())
-
-  // check if this word already exists
-  const wordSearch = await prismaClient.word.findFirst({where: {word}})
-  if(wordSearch) return res.status(400).json({error: `Word '${word}' is already exist!`})
-
-  // create translations arrays
-  let translationsIdsToConnectTo: {id: number}[] = []
-  let translationsToCreate: Prisma.TranslationCreateInput[] = []
-
-  // check if similar translations exist
-  for(let translation of translations) {
-    const translationDb = await prismaClient.translation.findFirst({where: {translation}})
-    if(translationDb) {
-      translationsIdsToConnectTo.push({id: translationDb.id})
-    }
-    else {
-      translationsToCreate.push({translation})
-    }
-  }
-
-  // create a new word
-  const wordDb = await prismaClient.word.create({
-    data: {
-      word: word.toLowerCase(),
-      translations: {
-        create: translationsToCreate,
-        connect: translationsIdsToConnectTo
-      }
-    }
-  })
-
+  const result = await addWord(word, translationsString)
   // return result
-  res.status(200).json(wordDb)
+  res.status(200).json(result)
 }
