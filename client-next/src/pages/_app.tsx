9f8d664dab71099ecb17ef0cdb8257ea1a4972ba
Buor <buor0@mail.ru>
2022-07-11 22:59:27 +0300

    Remove semicolons from _app.tsx

--- client-next/src/pages/_app.tsx
+++ client-next/src/pages/_app.tsx
@@ -1,12 +1,12 @@
-import "../styles/global.css";
-import type { AppProps } from "next/app";
-import "@/assets/styles/styles.scss"
+import '../styles/global.css'
+import type {AppProps} from 'next/app'
+import '@/assets/styles/styles.scss'
 import Navbar from '@/components/layout/Navbar'
 import {QueryClient, QueryClientProvider} from 'react-query'
 
 const queryClient = new QueryClient()
 
-export default function MyApp({ Component, pageProps }: AppProps) {
+export default function MyApp({Component, pageProps}: AppProps) {
   return <QueryClientProvider client={queryClient}>
     <div className={'container'}>
       <Navbar/>
