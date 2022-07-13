eb2758fd922f78fe81877166a376ef21daccacb3
Buor <buor0@mail.ru>
2022-07-13 21:49:31 +0300

    Add many words route has been implemented

--- client-next/src/pages/api/word/addMany.ts
+++ client-next/src/pages/api/word/addMany.ts
@@ -1,20 +1,37 @@
 import {NextApiRequest, NextApiResponse} from 'next'
 import {z} from 'zod'
+import {addWord} from '@/core/backend/word'
 
 export default async function addManyWords(req: NextApiRequest, res: NextApiResponse) {
-  const words = req.body
+  const reqWordsData = req.body
 
-  const validationSchema = z.object({
+  const wordsValidationSchema = z.object({
       word: z.string(),
       translationsString: z.string()
     }
   ).array()
 
-  const validationResult = validationSchema.safeParse(words)
+  const validationResult = wordsValidationSchema.safeParse(reqWordsData)
 
-  if(!validationResult.success) {
+  if (!validationResult.success) {
     res.status(400).json('Error! Wrong data! Message: ' + validationResult.error)
   } else {
-    res.status(200).json(validationResult.data)
+    const wordsData = validationResult.data
+    let errors: string[] = []
+
+    // check if some of these words already exists
+    for (const {word, translationsString} of wordsData) {
+      const addWordResult = await addWord(word, translationsString)
+
+      if (addWordResult.isError) {
+        errors.push(addWordResult.errorMessage)
+      }
+    }
+
+    let responseMessage = errors.length
+      ? `Words creation finished, but there were some errors: ${errors.join(', ')}`
+      : 'Words creation has been finished!'
+
+    return res.status(201).json(responseMessage)
   }
 }
