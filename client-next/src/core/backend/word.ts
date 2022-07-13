2933ae30c04ac1592dd604729130a143654612da
Buor <buor0@mail.ru>
2022-07-13 21:48:50 +0300

    Rework word addition
    Now if user tries to add an existing word,
    there won't be an error. Instead, the existing
    word will receive new translations

--- client-next/src/core/backend/word.ts
+++ client-next/src/core/backend/word.ts
@@ -1,5 +1,6 @@
 import {prismaClient} from '../../../prisma/prismaInstance'
 import {Prisma} from '@prisma/client'
+import {IWord} from '../../../prisma/prismaTypes'
 
 export interface IMutationAddWord {
   translationsString: string,
@@ -8,43 +9,80 @@ export interface IMutationAddWord {
 
 export type IMutationAddWords = IMutationAddWord[]
 
-export const addWord = async (word: string, translationsString: string) => {
+interface IAddWordResultSuccess {
+  isError: false
+  data: IWord
+}
 
-  word = word.toLowerCase()
+interface IAddWordResultError {
+  isError: true
+  errorMessage: string
+}
 
-  const translations = translationsString
-    .split(',')
-    .map(tr => tr.trim().toLowerCase())
+export const addWord = async (word: string, translationsString: string): Promise<IAddWordResultSuccess | IAddWordResultError> => {
+  try {
+    word = word.toLowerCase()
 
-  // check if this word already exists
-  const wordSearch = await prismaClient.word.findFirst({where: {word}})
-  if(wordSearch) return {isError: true, errorMessage: `Word '${word}' is already exist!`}
+    const translations = translationsString
+      .split(',')
+      .map(tr => tr.trim().toLowerCase())
 
-  // create translations arrays
-  let translationsIdsToConnectTo: {id: number}[] = []
-  let translationsToCreate: Prisma.TranslationCreateInput[] = []
+    // check if this word already exists, if it is, we're working with this word
+    const searchedWord = await prismaClient.word.findFirst({where: {word}})
 
-  // check if similar translations exist
-  for(let translation of translations) {
-    const translationDb = await prismaClient.translation.findFirst({where: {translation}})
-    if(translationDb) {
-      translationsIdsToConnectTo.push({id: translationDb.id})
-    }
-    else {
-      translationsToCreate.push({translation})
-    }
-  }
+    // create translations arrays
+    let translationsIdsToConnectTo: { id: number }[] = []
+    let translationsToCreate: Prisma.TranslationCreateInput[] = []
 
-  // create a new word
-  const wordDb = await prismaClient.word.create({
-    data: {
-      word: word.toLowerCase(),
-      translations: {
-        create: translationsToCreate,
-        connect: translationsIdsToConnectTo
+    // check if similar translations exist
+    for (let translation of translations) {
+      const translationDb = await prismaClient.translation.findFirst({where: {translation}})
+      if (translationDb) {
+        translationsIdsToConnectTo.push({id: translationDb.id})
+      } else {
+        translationsToCreate.push({translation})
       }
     }
-  })
 
-  return {isError: false, data: wordDb}
+    // create a new word if searchedWord is null, otherwise add translations to searchedWord
+    if (!searchedWord) {
+      const wordDb = await prismaClient.word.create({
+        data: {
+          word: word.toLowerCase(),
+          translations: {
+            create: translationsToCreate,
+            connect: translationsIdsToConnectTo
+          }
+        },
+        select: {
+          id: true,
+          word: true,
+          translations: true
+        }
+      })
+      return {isError: false, data: wordDb}
+
+    } else {
+      const savedWord = await prismaClient.word.update({
+        where: {
+          id: searchedWord.id
+        },
+        data: {
+          translations: {
+            create: translationsToCreate,
+            connect: translationsIdsToConnectTo
+          }
+        },
+        select: {
+          id: true,
+          word: true,
+          translations: true
+        }
+      })
+
+      return {isError: false, data: savedWord}
+    }
+  } catch (e) {
+    return {isError: true, errorMessage: (e as Error).message}
+  }
 }
