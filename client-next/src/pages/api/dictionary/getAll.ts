bd21e4a5e7ad2d61e65830a4c45965c9c4f808db
Buor <buor0@mail.ru>
2022-07-15 16:32:12 +0300

    Fix 'get all' dictionaries endpoint

--- client-next/src/pages/api/dictionary/getAll.ts
+++ client-next/src/pages/api/dictionary/getAll.ts
@@ -1,5 +1,7 @@
 import {prismaClient} from '../../../../prisma/prismaInstance'
+import {NextApiRequest, NextApiResponse} from 'next'
 
-export default async function getAllDictionaries() {
-  return await prismaClient.dictionary.findMany()
+export default async function getAllDictionaries(req: NextApiRequest, res: NextApiResponse) {
+  const allDictionaries = await prismaClient.dictionary.findMany()
+  return res.status(200).json(allDictionaries)
 }
