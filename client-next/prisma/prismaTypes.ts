0cc1d09b538e5333f6302c0c764307d34b063727
Buor <buor0@mail.ru>
2022-07-15 15:57:36 +0300

    Add IDictionary interface to prismaTypes

--- client-next/prisma/prismaTypes.ts
+++ client-next/prisma/prismaTypes.ts
@@ -8,3 +8,11 @@ export interface ITranslation {
   id: number
   translation: string
 }
+
+export interface IDictionary {
+  id: number
+  words: IWord[]
+  difficulty: number
+  choosed: boolean
+  wordsCount: number
+}
